880ff9bfeefe1d453ffde205f485fe8d28c38943
Joseph Kwon <kwonjosepho@gmail.com>
2018-03-01 17:19:24 -0800

    (feat) add mongodb functionality for heroku

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test');
 
 var db = mongoose.connection;
 
